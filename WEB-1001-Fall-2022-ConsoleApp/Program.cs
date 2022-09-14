b918ddf8d4425393c1f6dcbb6dd1dde566965ebf
Brent Ritchie <ritchie.brent@gmail.com>
2022-09-14 18:59:59 -0400

    Add project files.

---
+++ WEB-1001-Fall-2022-ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WEB_1001_Fall_2022_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
